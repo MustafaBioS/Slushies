af76f89a9ba10c37256758232415d9283e6f256d
MustafaBioS <mustaafahany@gmail.com>
2026-02-07 21:37:53 +0200

    ADD - More Info

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -1,5 +1,5 @@
 import './style.css'
-import * as THREE from 'three';
+// import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import { MeshStandardMaterial } from 'three';
 
