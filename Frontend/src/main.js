05dc8eaf46cbf89efc903e9ae1e882e5acc8e769
MustafaBioS <mustaafahany@gmail.com>
2026-02-09 01:05:56 +0200

    ADD - Requirements

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -1,70 +1,21 @@
-import './style.css'
-// import * as THREE from 'three';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
-import { MeshStandardMaterial } from 'three';
-
-const scene = new THREE.Scene();
-
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
-const renderer = new THREE.WebGLRenderer({
-    canvas: document.querySelector("#BG"),
-    alpha: true,
-});
+const flag = document.querySelector('.flag');
+const q = document.querySelectorAll('.q');
 
 
-renderer.setPixelRatio( window.devicePixelRatio );
-renderer.setSize( window.innerWidth, window.innerHeight);
+flag.addEventListener('click', ()=> {
+    window.location.href = "https://hackclub.com/";
+})
 
-camera.position.setZ(9);
+q.forEach(qs => {
+    qs.addEventListener('click', (toggleFAQ))
+})
 
-const ambientLight = new THREE.AmbientLight(0xffffff, 1);
-scene.add(ambientLight);
+function toggleFAQ(e) {
+    const current = e.currentTarget;
 
-const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
-directionalLight.position.set(5, 10, 7.5);
-scene.add(directionalLight);
+    document.querySelectorAll('.q').forEach(q => {
+        if (q !== current) q.classList.remove('open');
+    });
 
-
-
-renderer.render( scene, camera ); 
-
-let slushie;
-
-const loader = new GLTFLoader();
-
-loader.load('../models/slushie.glb', (gltf) => {
-    slushie = gltf.scene;
-    slushie.position.set(0, 0, 0);
-    slushie.scale.set(0.5, 0.5, 0.5);
-    slushie.rotation.z = 75;
-    scene.add(slushie);
-}, undefined, (error) => {
-    console.error("error");
-});
-
-window.addEventListener('resize', () => {
-    renderer.setPixelRatio( window.devicePixelRatio );
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-});
-
-function moveCam() {
-    const t = document.body.getBoundingClientRect().top;
-
-    camera.position.z = 9 + t * -0.01;;
-    slushie.position.x = t * -0.03;
+    current.classList.toggle('open');
 }
-
-document.body.onscroll = moveCam;
-
-function animate() {
-    requestAnimationFrame( animate );
-
-    slushie.rotation.y += 0.01;
-
-    renderer.render( scene, camera );
-}
-
-animate();
